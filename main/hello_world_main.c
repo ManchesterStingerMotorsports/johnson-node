459b91df0d5ad581f42305d4c2eadaaa4ae9bff4
Trovix <james.platt@live.co.uk>
2026-04-13 22:39:19 +0100

    code

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,8 +1,4 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
+//Johnson node
 
 #include <stdio.h>
 #include <inttypes.h>
